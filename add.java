35f5a697a8c130a01ac4a3b93ade1c4368128f0a
sukiharan <sukiharan@gmail.com>
2023-11-10 05:43:08 +0000

    new add java code

---
+++ add.java
@@ -0,0 +1,11 @@
+#sample add java code copied from chatgpt
+public class AddNumbers {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 10;
+        int sum = number1 + number2;
+
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
+
