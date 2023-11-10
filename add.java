bfacc55bfe2c2dbaed55ad96377d6423d829b7b6
sukiharan <sukiharan@gmail.com>
2023-11-10 06:26:52 +0000

    complex 3 no add java

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 #sample add java code copied from chatgpt
 import java.util.Scanner;
 
-public class AddNumbers {
+public class AddThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -11,9 +11,12 @@ public class AddNumbers {
         System.out.print("Enter the second number: ");
         int number2 = scanner.nextInt();
 
-        int sum = number1 + number2;
+        System.out.print("Enter the third number: ");
+        int number3 = scanner.nextInt();
 
-        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+        int sum = number1 + number2 + number3;
+
+        System.out.println("The sum of " + number1 + ", " + number2 + ", and " + number3 + " is: " + sum);
 
         scanner.close();
     }
